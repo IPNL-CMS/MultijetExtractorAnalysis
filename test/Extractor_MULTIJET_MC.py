5e3425eddf0ffe3ead2e21e69f981a5dc4730356
Anne-Laure Pequegnot <al.pequegnot@ipnl.in2p3.fr>
2013-12-02 18:35:58 +0100

    function to read the T3 rootfiles list

--- test/Extractor_MULTIJET_MC.py
+++ test/Extractor_MULTIJET_MC.py
@@ -1,7 +1,11 @@
 from Extractor_MULTIJET_common import *
 
+with open('../../../PatTopProduction/multijet_QCD_HT_fewEvents.list') as f:
+  files = f.readlines()
+
 process = createExtractorProcess(True, True, useShiftCorrectedMET = True, globalTag = "START53_V23")
 
-process.source.fileNames = cms.untracked.vstring( 'file:/gridgroup/cms/pequegnot/CMSSW/CMSSW_5_3_9_patch2/src/PatTopProduction/patTuple.root'
-    )
-process.maxEvents.input = 5000
+process.source.fileNames = cms.untracked.vstring(files)
+#process.source.fileNames = cms.untracked.vstring( 'file:/gridgroup/cms/pequegnot/CMSSW/CMSSW_5_3_9_patch2/src/PatTopProduction/patTuple.root'
+    #)
+process.maxEvents.input = -1
