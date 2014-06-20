4dc5abec4e1dd71baece6236aadaad2418b479e5
Anne-Laure Pequegnot <a-l.pequegnot@ipnl.in2p3.fr>
2014-06-20 09:37:30 +0200

    Test the code on new file

--- test/Extractor_MULTIJET_MC.py
+++ test/Extractor_MULTIJET_MC.py
@@ -1,11 +1,11 @@
 from Extractor_MULTIJET_common import *
 
-with open('../../../PatTopProduction/multijet_QCD_HT_fewEvents.list') as f:
-  files = f.readlines()
+#with open('../../../PatTopProduction/multijet_QCD_HT_fewEvents.list') as f:
+#  files = f.readlines()
 
 process = createExtractorProcess(True, True, useShiftCorrectedMET = True, globalTag = "START53_V27")
 
-process.source.fileNames = cms.untracked.vstring(files)
-#process.source.fileNames = cms.untracked.vstring( 'file:/gridgroup/cms/pequegnot/CMSSW/CMSSW_5_3_9_patch2/src/PatTopProduction/patTuple.root'
-    #)
+#process.source.fileNames = cms.untracked.vstring(files)
+process.source.fileNames = cms.untracked.vstring( '/store/user/apequegn/QCD_HT-100To250_TuneZ2star_8TeV-madgraph-pythia/QCD_HT-100To250_START53_V7A_13Mar14-v1/c5f9c59e100f883a59cec8d8908af608/patTuple_976_1_4Lv.root'
+    )
 process.maxEvents.input = -1
--- test/Extractor_MULTIJET_data.py
+++ test/Extractor_MULTIJET_data.py
@@ -16,7 +16,7 @@ from Extractor_MULTIJET_common import *
 process = createExtractorProcess(False, True, useShiftCorrectedMET = True, globalTag = options.globalTag)
 
 
-process.source.fileNames = cms.untracked.vstring( '/store/user/apequegn/JetHT/JetHT_Run2012B-22Jan2013_04Dec13-v1/2b71cc75519c435218af9c37f5843477/patTuple_89_2_h2v.root'
+process.source.fileNames = cms.untracked.vstring( 'file:/gridgroup/cms/pequegnot/CMSSW/CMSSW_5_3_9_patch2/src/PatTopProduction/patTuple.root'
 )
 
 process.maxEvents.input = 10000
