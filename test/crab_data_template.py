4dbe73e6b62eb147dbb41f0e92b2e7368bc79099
Anne-Laure Pequegnot <a-l.pequegnot@ipnl.in2p3.fr>
2016-02-10 10:22:32 +0100

    remove ignoreLocality and whitelist

--- test/crab_data_template.py
+++ test/crab_data_template.py
@@ -22,10 +22,10 @@ config.Data.splitting = "LumiBased"
 config.Data.unitsPerJob = 15
 config.Data.lumiMask = "@golden_json_file@"
 # crab bug: remove ignoreLocality and whitelist ; see https://hypernews.cern.ch/HyperNews/CMS/get/computing-tools/958/2/1/1/1/1/1/1/1/1/1/1/1/1/1/1.html
-config.Data.ignoreLocality = True
+#config.Data.ignoreLocality = True
 
 # Site section
 config.Site.storageSite = "T3_FR_IPNL"
 #config.Site.whitelist = ['T0_CH_CERN_Disk', 'T1_IT_CNAF_Buffer', 'T1_IT_CNAF_Disk', 'T1_IT_CNAF_MSS', 'T2_BE_UCL', 'T2_CH_CERN', 'T2_DE_DESY', 'T2_FR_IPHC', 'T2_IT_Legnaro', 'T2_UK_London_IC']
-config.Site.whitelist = ['T2_BE_UCL', 'T2_CH_CERN', 'T2_DE_DESY', 'T2_FR_IPHC', 'T2_IT_Legnaro', 'T2_UK_London_IC']
+#config.Site.whitelist = ['T2_BE_UCL', 'T2_CH_CERN', 'T2_DE_DESY', 'T2_FR_IPHC', 'T2_IT_Legnaro', 'T2_UK_London_IC']
 
