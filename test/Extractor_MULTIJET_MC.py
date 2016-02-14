c1c73a4bb88250a45aa484997b926fa119f6ba77
Anne-Laure Pequegnot <a-l.pequegnot@ipnl.in2p3.fr>
2016-02-14 18:49:20 +0100

    Update for 76X usage

--- test/Extractor_MULTIJET_MC.py
+++ test/Extractor_MULTIJET_MC.py
@@ -6,19 +6,14 @@ from Extractor_MULTIJET_common import *
 #with open('../../../PatTopProduction/multijet_QCD_HT_fewEvents.list') as f:
 #  files = f.readlines()
 
-#process = createExtractorProcess(True, True, useShiftCorrectedMET = True, globalTag = "PHYS14_25_V2")
-#process = createExtractorProcess(True, True, useShiftCorrectedMET = True, globalTag = "74X_mcRun2_asymptotic_v2") # Summer15_25nsV5
-process = createExtractorProcess(True, True, useShiftCorrectedMET = True, globalTag = "74X_mcRun2_asymptotic_v4") # Summer15_25nsV6
+
+process = createExtractorProcess(True, True, useShiftCorrectedMET = True, globalTag = "76X_mcRun2_asymptotic_v12")
 
 #process.source.fileNames = cms.untracked.vstring(files)
 process.source.fileNames = cms.untracked.vstring(
-        #'file:patTuple_1.root'
-        #'/store/user/apequegn/RelValTTbar_13/RelValTTbar_13TeV_18Mar15-v1/150318_170818/0000/patTuple_1.root' # relVal
-        #'/store/user/apequegn/QCD_Pt-15TTo7000_TuneZ2star-Flat_13TeV_pythia6/QCD_Pt-15TTo7000_TuneZ2star-Flat_13TeV_pythia6_25Jun15-v1/150625_204806/0000/patTuple_10.root' 
-        #'/store/mc/RunIISpring15DR74/QCD_Pt-15TTo7000_TuneZ2star-Flat_13TeV_pythia6/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v1/00000/0AAC75BF-39FE-E411-BD62-0025905A60A0.root' 
-        #'/store/mc/RunIISpring15DR74/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/MINIAODSIM/Asympt50nsRaw_MCRUN2_74_V9A-v3/50000/CC91FB27-F308-E511-A212-00266CFBE43C.root' 
-        #'/store/mc/RunIISpring15DR74/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/MINIAODSIM/Asympt25nsRaw_MCRUN2_74_V9-v3/00000/08547C6F-CD08-E511-81F2-0025905A60AA.root' 
-        '/store/mc/RunIISpring15MiniAODv2/QCD_HT200to300_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/10000/001387CB-666F-E511-8F4A-0025907FD24A.root' 
+        #'file:patTuple_1.root' # local file
+        #'/store/mc/RunIIFall15MiniAODv2/QCD_Pt-15to7000_TuneCUETP8M1_Flat_13TeV_pythia8/MINIAODSIM/PU25nsData2015v1_magnetOn_76X_mcRun2_asymptotic_v12-v1/00000/02B9457D-47BC-E511-B18F-E41D2D08DE30.root' # QCD Pt Flat test
+        '/store/mc/RunIIFall15MiniAODv2/QCD_HT2000toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/10000/04A66FE4-47B9-E511-85C6-002590DB9216.root' # QCD HT test
 
     )
 process.maxEvents.input = 1000
--- test/setup_crab3_env.sh
+++ test/setup_crab3_env.sh
@@ -1,3 +1,3 @@
-export SCRAM_ARCH=slc6_amd64_gcc491
+export SCRAM_ARCH=slc6_amd64_gcc493
 source /cvmfs/cms.cern.ch/crab3/crab.sh
 #source /gridgroup/cms/pequegnot/bin/crab-env-bootstrap.sh
